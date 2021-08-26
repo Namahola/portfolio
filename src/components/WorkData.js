6abd23073e10206f10e1c456dc1cc03ddeda87bd
Nanna Laursen <nannalaursen@gmail.com>
2021-08-26 07:56:45 +0200

    work page images

---
+++ src/components/WorkData.js
@@ -0,0 +1,75 @@
+import leonardo from "../img/leonardo1Full.png";
+import leonardo2 from "../img/leonardo2Full.png";
+import verdensende from "../img/Udklip.PNG";
+import verdensende2 from "../img/verdensendekontaktFull.png";
+import vikingfood from "../img/vikingFull.png";
+import vikingmenu from "../img/vikingmenu2Full.png";
+import excalibur from "../img/excaliburFull.png";
+import movieDB from "../img/movieDBFull.png";
+import periodic from "../img/PeriodicTableFull.png";
+import valorant from "../img/valorant1Full.png";
+
+export const WorkData = [
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: leonardo,
+    id: "image1",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: leonardo2,
+    id: "image2",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: verdensende,
+    id: "image3",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: verdensende2,
+    id: "image4",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: vikingfood,
+    id: "image5",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: vikingmenu,
+    id: "image6",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: excalibur,
+    id: "image7",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: movieDB,
+    id: "image8",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: periodic,
+    id: "image9",
+  },
+  {
+    title: "Leonardoskolen",
+    description: "something",
+    src: valorant,
+    id: "image10",
+  },
+];
+
+export default WorkData;
--- src/components/work.js
+++ src/components/work.js
@@ -1,7 +1,22 @@
 import React from "react";
+import WorkData from "./WorkData.js";
 
 function Work() {
-  return <div></div>;
+  return (
+    <div className="work">
+      {WorkData.map((image) => (
+        <div>
+          <h1>{image.title}</h1>
+          <img
+            className="work-image"
+            src={image.src}
+            key={image.id}
+            alt="billede"
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Work;
